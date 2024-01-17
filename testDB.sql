3bf2814e70bb70c5310338c09d53fe722ad586b6
Derp88 <62079853+Derp88@users.noreply.github.com>
2024-01-16 23:14:00 -0500

    Init DB

---
+++ testDB.sql
@@ -0,0 +1,10 @@
+CREATE TABLE PLAYER (
+playerID int,
+FirstName varchar(255),
+LastName varchar(255),
+Sport varchar(255)
+);
+
+INSERT INTO PLAYER (playerID,FirstName,LastName,Sport) VALUES (2,'Gary','Anderson','Biz');
+
+SELECT * FROM PLAYER;
